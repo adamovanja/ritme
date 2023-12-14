929bfbc3c2ec167a18ac37ed25fa95d84fcc0689
Anja Adamov <57316423+adamovanja@users.noreply.github.com>
2023-12-14 14:00:43 +0100

    more tests

--- q2_time/tests/test_transform_features.py
+++ q2_time/tests/test_transform_features.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
@@ -5,6 +7,7 @@ from qiime2.plugin.testing import TestPluginBase
 from scipy.stats.mstats import gmean
 from skbio.stats.composition import ilr
 
+from q2_time.feature_space._process_train import process_train
 from q2_time.feature_space.transform_features import (
     PSEUDOCOUNT,
     alr,
@@ -113,3 +116,59 @@ class TestTransformFeatures(TestPluginBase):
             ValueError, "Method FancyTransform is not implemented yet."
         ):
             transform_features(self.ft, "FancyTransform")
+
+
+class TestProcessTrain(TestPluginBase):
+    package = "q2_time.test"
+
+    def setUp(self):
+        super().setUp()
+        self.config = {"data_transform": None, "data_alr_denom_idx": False}
+        self.train_val = pd.DataFrame(
+            {
+                "host_id": ["c", "b", "c", "a"],
+                "target": [1, 2, 1, 2],
+                "F0": [0.12, 0.23, 0.33, 0.44],
+                "F1": [0.1, 0.2, 0.3, 0.4],
+            },
+            index=["SR1", "SR2", "SR3", "SR4"],
+        )
+        self.target = "target"
+        self.host_id = "host_id"
+        self.seed_data = 0
+
+
+def _assert_called_with_df(mock, expected_df, *expected_args):
+    mock.assert_called_once()
+    args, _ = mock.call_args
+    pd.testing.assert_frame_equal(args[0], expected_df)
+    for expected, actual in zip(expected_args, args[1:]):
+        assert expected == actual, f"Expected {expected}, but got {actual}"
+
+
+@patch("q2_time.feature_space._process_train.transform_features")
+@patch("q2_time.feature_space._process_train.split_data_by_host")
+def test_process_train(self, mock_split_data_by_host, mock_transform_features):
+    # Arrange
+    ls_ft = ["F0", "F1"]
+    ft = self.train_val[ls_ft]
+    mock_transform_features.return_value = ft
+    mock_split_data_by_host.return_value = (
+        self.train_val.iloc[:2, :],
+        self.train_val.iloc[2:, :],
+    )
+
+    # Act
+    X_train, y_train, X_val, y_val = process_train(
+        self.config, self.train_val, self.target, self.host_id, self.seed_data
+    )
+
+    # Assert
+    _assert_called_with_df(mock_transform_features, ft, None, False)
+    _assert_called_with_df(
+        mock_split_data_by_host,
+        self.train_val[[self.target, self.host_id] + ls_ft],
+        "host_id",
+        0.8,
+        0,
+    )
