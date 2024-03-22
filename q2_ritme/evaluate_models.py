FIX nn_class rmse calculation

q2_ritme/evaluate_models.py
@@ -13,9 +13,6 @@ from sklearn.metrics import mean_squared_error
 from q2_ritme.feature_space.transform_features import transform_features
 from q2_ritme.model_space._static_trainables import NeuralNet
 
-# 30.437 is avg. number of days per month
-DAYS_PER_MONTH = 30.437
-
 
 def _get_checkpoint_path(result: Result) -> str:
     """
@@ -176,24 +173,21 @@ def plot_rmse_over_experiments(preds_dic, save_loc, dpi=400):
     plt.show()
 
 
-def plot_rmse_over_time(
-    preds_dic, ls_model_types, save_loc, days_per_month=DAYS_PER_MONTH, dpi=300
-):
+def plot_rmse_over_time(preds_dic, ls_model_types, save_loc, dpi=300):
     """
     Plot RMSE over true time bins for the first model type in ls_model_types.
 
     Parameters:
     preds_dic (dict): Dictionary containing predictions for each model type.
     ls_model_types (list): List of model types.
-    days_per_month (float): Average number of days per month to use for binning.
     dpi (int): Resolution of the plot.
     """
     for model_type in ls_model_types:
         pred_df = preds_dic[model_type]
         split = None
 
-        # Bin true columns by months
-        pred_df["group"] = np.round(pred_df["true"] / days_per_month, 0).astype(int)
+        # Bin true columns
+        pred_df["group"] = np.round(pred_df["true"], 0).astype(int)
 
         # Calculate RMSE for each group
         grouped_ser = pred_df.groupby(["group"]).apply(calculate_rmse)

q2_ritme/model_space/_static_trainables.py
@@ -192,28 +192,45 @@ class NeuralNet(LightningModule):
             x = layer(x)
         return x
 
+    def _prepare_predictions(self, predictions):
+        # todo: add ordinal regression option
+        if self.nn_type == "regression":
+            return predictions
+        elif self.nn_type == "classification":
+            return torch.argmax(predictions, dim=1).float()
+
     def training_step(self, batch, batch_idx):
         inputs, targets = batch
         predictions = self(inputs).squeeze()
+
+        # loss: cross-entropy or mse
         loss = self.loss_fn(predictions, targets)
-        rmse = torch.sqrt(loss)
         self.log(
             "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
+
+        # rmse
+        pred_rmse = self._prepare_predictions(predictions)
+        rmse = torch.sqrt(nn.functional.mse_loss(pred_rmse, targets))
         self.log(
             "train_rmse", rmse, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
+
         return loss
 
     def validation_step(self, batch, batch_idx):
         inputs, targets = batch
-        # ! squeeze could be causing problems
         predictions = self(inputs).squeeze()
+
+        # loss: cross-entropy or mse
         loss = self.loss_fn(predictions, targets)
-        rmse = torch.sqrt(loss)
         self.log(
             "val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
+
+        # rmse
+        pred_rmse = self._prepare_predictions(predictions)
+        rmse = torch.sqrt(nn.functional.mse_loss(pred_rmse, targets))
         self.log(
             "val_rmse", rmse, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )