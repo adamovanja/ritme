b67106b68168e52df33bd38c45066eee054076a7
Anja Adamov <57316423+adamovanja@users.noreply.github.com>
2025-03-12 13:17:42 +0100

    add tests

---
+++ ritme/tests/test_config_files.py
@@ -0,0 +1,137 @@
+import json
+import os
+import unittest
+
+import pandas as pd
+from parameterized import parameterized
+
+from ritme.split_train_test import split_train_test
+
+
+class TestConfigFiles(unittest.TestCase):
+    """
+    Verify that experiment config files suggested in config folder match the
+    newest software code
+    """
+
+    def setUp(self):
+        super().setUp()
+        base_dir = os.path.dirname(__file__)
+        self.path_to_config = os.path.join(base_dir, "..", "..", "config")
+
+        self.data_rel = pd.DataFrame(
+            {
+                "host_id": ["c", "b", "c", "a"],
+                "age_months": [1, 2, 5, 7],
+                "covariate": [0, 1, 0, 1],
+                "F0": [0.83333333333333333, 0.25, 1.0, 1.0],
+                "F1": [0.16666666666666666, 0.75, 0.0, 0.0],
+            }
+        )
+        self.data_rel.index = ["SR1", "SR2", "SR3", "SR4"]
+        self.md = self.data_rel[["host_id", "age_months", "covariate"]]
+        self.ft_rel = self.data_rel[["F0", "F1"]]
+
+    @parameterized.expand(
+        [
+            "run_config.json",
+            "run_config_whparams.json",
+        ]
+    )
+    def test_split_train_test(self, filename_json):
+        config_path = os.path.join(self.path_to_config, filename_json)
+        with open(config_path) as f:
+            run_config = json.load(f)
+
+        try:
+            split_train_test(
+                md=self.md,
+                ft=self.ft_rel,
+                group_by_column=run_config["group_by_column"],
+                train_size=0.8,
+                seed=run_config["seed_data"],
+            )
+        except Exception as e:
+            self.fail(
+                f"Failed with {filename_json} " f"and this exception was raised: {e}"
+            )
+
+    @parameterized.expand(
+        [
+            "run_config.json",
+            "run_config_whparams.json",
+        ]
+    )
+    def test_find_best_model_config_models_listed(self, filename_json):
+        config_path = os.path.join(self.path_to_config, filename_json)
+        with open(config_path) as f:
+            run_config = json.load(f)
+
+        # Check if all of models_list are valid (i.e., no unknown models)
+        config_models = set(run_config["ls_model_types"])
+        allowed_models = {
+            "linreg",
+            "trac",
+            "xgb",
+            "nn_reg",
+            "nn_class",
+            "nn_corn",
+            "rf",
+        }
+        self.assertTrue(
+            config_models.issubset(allowed_models),
+            f"Unknown models found: {config_models - allowed_models}",
+        )
+
+    @parameterized.expand(
+        [
+            ("linreg", {"alpha": ["min", "max"], "l1_ratio": ["min", "max"]}),
+            (
+                "rf",
+                {
+                    "n_estimators": ["min", "max", "step"],
+                    "max_depth": None,
+                    "min_samples_split": ["min", "max", "log"],
+                    "min_weight_fraction_leaf": ["min", "max", "log"],
+                    "min_samples_leaf": ["min", "max", "log"],
+                    "max_features": None,
+                    "min_impurity_decrease": ["min", "max", "log"],
+                    "bootstrap": None,
+                },
+            ),
+            (
+                "nn_all_types",
+                {
+                    "n_hidden_layers": ["min", "max", "step"],
+                    "learning_rate": None,
+                    "batch_size": None,
+                    "epochs": None,
+                    "n_units_hl": None,
+                },
+            ),
+            (
+                "xgb",
+                {
+                    "max_depth": ["min", "max"],
+                    "min_child_weight": ["min", "max"],
+                    "subsample": ["min", "max"],
+                    "eta": ["min", "max"],
+                    "num_parallel_tree": ["min", "max", "step"],
+                },
+            ),
+            (
+                "trac",
+                {"lambda": ["min", "max", "log"]},
+            ),
+        ]
+    )
+    def test_custom_template_hparams_for_find_best_model(self, model, hparams):
+        config_path = os.path.join(self.path_to_config, "run_config_whparams.json")
+        with open(config_path) as f:
+            run_config = json.load(f)
+
+        for k, v in hparams.items():
+            self.assertIn(k, run_config["model_hyperparameters"][model])
+            if v is not None:
+                for vi in v:
+                    self.assertIn(vi, run_config["model_hyperparameters"][model][k])
